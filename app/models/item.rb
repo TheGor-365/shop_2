cafc72f6ec375fdd94e927e7fc165a8309c27495
Gor <7495500@gmail.com>
2020-08-21 01:27:56 +0300

    Add callback to model

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,10 @@
 class Item < ApplicationRecord
+  validates :price, numericality: { greater_than: 0, allow_nil: true }
+  validates :name, :description, presence: true
+
+  after_initialize { puts 'initialize' } # Item.new, Item.first
+  after_save       { puts 'save' } # Item.create, Item.save
+  after_create     { puts 'create' } # Item.create
+  after_update     { puts 'update' } # Item.update
+  after_destroy    { puts 'destroy' } # Item.last.destroy
 end
