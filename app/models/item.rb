f7a7e845980d2871d8823f49211e26f98f5d3728
Gor <7495500@gmail.com>
2020-08-22 02:49:15 +0300

    Add partials

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,11 @@ class Item < ApplicationRecord
   validates :price, numericality: { greater_than: 0, allow_nil: true }
   validates :name, :description, presence: true
 
+  # has_and_belongs_to_many :carts
+  has_many :positions
+  has_many :carts, through: :positions
+  has_many :comments, as: :commentable
+
   after_initialize { puts 'initialize' } # Item.new, Item.first
   after_save       { puts 'save' } # Item.create, Item.save
   after_create     { puts 'create' } # Item.create
---
+++ db/migrate/20200821222708_drop_table_carts_items.rb
@@ -0,0 +1,5 @@
+class DropTableCartsItems < ActiveRecord::Migration[6.0]
+  def change
+    drop_table :carts_items
+  end
+end
